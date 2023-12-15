ae050f908686e2d5d0dda063dd9f6878c6a2c8c0
tjehdgur1500 <donghyeok.dev@gmail.com>
2023-12-15 10:14:37 +0900

    플리 추가

--- src/common/music.ts
+++ src/common/music.ts
@@ -3,4 +3,6 @@ export const youtubePlayList = [
   'PLDKjtWiRDPL8gEVoG-AO2WDRdW6OEA8jG',
   'PLDKjtWiRDPL-guL-JI3hfuMdi6HMgBVAN',
   'OLAK5uy_l1yjiikmvAskEpIp6G07vpj1PPIRonyY0',
+  // Emile PlayList
+  'PLIWFAQtzU3vV8tSBGNzee738lQMEttT1k',
 ];
